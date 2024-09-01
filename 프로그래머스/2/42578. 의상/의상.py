5bf626aca8133f9d275ba69f9b7d522678420133
Jong hyeon Jo <97875266+jonghyeonjo99@users.noreply.github.com>
2024-09-01 20:36:01 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+def solution(clothes):
+    answer = 1
+    dic = dict()
+
+    for cloth, Type in clothes:
+        if(dic.get(Type) == None ):
+            dic[Type] = [cloth]
+            continue
+        dic[Type].append(cloth)    
+
+    for Type in dic:
+        answer *= len(dic[Type]) + 1
+
+    return answer - 1
